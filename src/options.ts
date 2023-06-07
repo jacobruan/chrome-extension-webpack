65ffe7b217db56345daf717555e0eeebd70290c4
Jacob Ruan <jacobruan@gmail.com>
2023-06-07 15:18:41 +0800

    add bootstrap and bootswath

--- src/options.ts
+++ src/options.ts
@@ -1,1 +1,3 @@
 import '../styles/options.scss';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
--- src/popup.ts
+++ src/popup.ts
@@ -1,4 +1,6 @@
 import '../styles/popup.scss';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 document.getElementById('go-to-options').addEventListener('click', () => {
   chrome.runtime.openOptionsPage();
