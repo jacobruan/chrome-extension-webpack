687b84036018e49404a8f48b31e255ec892e47fb
Jacob Ruan <jacobruan@gmail.com>
2023-06-12 15:49:08 +0800

    add jquery

--- src/popup.ts
+++ src/popup.ts
@@ -2,6 +2,8 @@ import '../styles/popup.scss';
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+import $ from 'jquery'
+
 document.getElementById('go-to-options').addEventListener('click', () => {
   chrome.runtime.openOptionsPage();
 });
